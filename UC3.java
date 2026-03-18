f41b410fae23a4a067f171a56b7741d3fedd04fd
sudhanvesh <sudhanvesh@gmail.com>
2026-03-18 10:27:29 +0530

    UC3HELLO

---
+++ UC3.java
@@ -0,0 +1,11 @@
+public class UC3 {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+}
